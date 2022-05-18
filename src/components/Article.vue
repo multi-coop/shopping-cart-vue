64fd9794e0acb269a3578e2f77f13c2390d51b40
lysiandon <don.lysiane@gmail.com>
2022-05-18 11:34:49 +0200

    added articles in store

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -1,5 +1,12 @@
 <template>
-    <div><h1>Article Item</h1></div>
+    <div class="wrapper">
+        <h1>Title : </h1>
+        <h2>Category : </h2>
+        <h2>Description : </h2>
+        <p></p>
+        <h2>Price : </h2>
+
+    </div>
 </template>
 
 <script>
@@ -10,8 +17,23 @@ export default{
 
         }
     },
+    // async mounted(){
+    //     const data = await fetch('https://fakestoreapi.com/products')
+    //     const response = await data.json()
+    //     console.log(response)    
+    // }
 }
 </script>
 
 <style scoped>
+.wrapper{
+    border: 1px solid black;
+    width: 80vw;
+    margin: auto;
+    box-sizing: border-box;
+}
+
+img{
+    max-width: 100%;
+}
 </style>
