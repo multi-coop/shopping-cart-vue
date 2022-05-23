fde9aa1b4c235ced87ae71393d3cca5b70b467b5
lysiandon <don.lysiane@gmail.com>
2022-05-23 12:22:00 +0200

    updated cart css

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,19 +1,22 @@
 <template>
-    <div>
-        <h1>Your cart</h1>
-
+    <div class="wrapper">
+        <div class="infos">
+            <h1>Your cart</h1>
         <h2 
            v-if="$store.state.addedArticlesCount > 0">
            You have {{$store.state.addedArticlesCount}} {{sg}}.
         </h2>
+
         <h2 
            v-if="$store.state.addedArticlesCount === 0">
            You don't have any articles yet.
-        </h2>
 
         <button 
-                @click="decrementCount"
-                v-if="$store.state.addedArticlesCount > 0">Empty the cart</button>
+               v-if="$store.state.addedArticlesCount > 0">Empty the Cart
+        </button>
+        </h2>
+        </div>
+
         <router-link
                    to="/">
             <button>Go shopping</button>
@@ -56,4 +59,29 @@ export default{
 </script>
 
 <style scoped>
+    .wrapper{
+      display: flex;
+      flex-direction: column;
+      flex-wrap: wrap;
+      border: 1px solid black;
+      height: 70vh;
+      width: 70vw;
+      margin: auto;
+      overflow: scroll;
+    }
+
+    h1{
+        text-align: center;
+    }
+
+    li{
+        list-style: none;
+    }
+    h2{
+        margin: auto;
+    }
+    button{
+        padding: 1%;
+        margin-top: 5%;
+    }
 </style>
